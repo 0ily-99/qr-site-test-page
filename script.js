502eb7e8eb720b94e3c687e4f3de16a18eb2dfd4
0ily-99 <victo.rr97169@gmail.com>
2025-04-28 22:21:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// This script runs when the page loads
+
+// Display a simple alert message to confirm JavaScript is working
+alert("JavaScript is running BABY.");
+
+// You can also log messages to the browser's developer console
+console.log("Test GitHub Pages site script loaded successfully.");
